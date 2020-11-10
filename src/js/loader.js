4321acaf2aa2c5d0bf6c7fdf4140c9a035069a12
Paulo <pauloeduardo2709@gmail.com>
2020-11-09 21:36:22 -0300

    Add loader

---
+++ src/js/loader.js
@@ -0,0 +1,11 @@
+// grid size
+const ROWS = 25;
+const COLUMNS = 50;
+
+// calculate unit
+const UNIT = 10;
+
+// load canvas
+const CANVAS = document.querySelector("canvas");
+CANVAS.width = UNIT * COLUMNS;
+CANVAS.height = UNIT * ROWS;
